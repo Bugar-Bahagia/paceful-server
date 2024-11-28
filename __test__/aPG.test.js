4405981d5db1e343807f8ad060f8cc8d7cdd4417
fathanabds <abdulshodiqfathan@gmail.com>
2024-11-28 10:50:56 +0700

    refactor: remove unused variables in aPG test file

--- __test__/aPG.test.js
+++ __test__/aPG.test.js
@@ -14,7 +14,7 @@ const userData = {
   dateOfBirth: '1999-11-27T03:10:39.262Z',
 };
 
-let userToken1, activity1Id, activity2Id, goal1Id, goal2Id;
+let userToken1, activity1Id, activity2Id, goal1Id;
 
 const walkingActivity = { typeName: 'walking', duration: 30, distance: 500, notes: 'morning walk in jakarta', activityDate: '2024-11-27T03:10:39.262Z' };
 
@@ -97,6 +97,7 @@ describe('GET /goals/:id should have dynamic currentValue ', () => {
       .then((response) => {
         const { body, status } = response;
         const caloriesBurned = calculateCalories(walkingActivity.typeName, walkingActivity.duration);
+
         activity1Id = body.id;
         expect(status).toBe(201);
         expect(body).toHaveProperty('id', activity1Id);
@@ -145,6 +146,7 @@ describe('GET /goals/:id should have dynamic currentValue ', () => {
       .then((response) => {
         const { body, status } = response;
         const caloriesBurned = calculateCalories(swimmingActivity.typeName, swimmingActivity.duration);
+
         activity2Id = body.id;
         expect(status).toBe(201);
         expect(body).toHaveProperty('id', activity2Id);
@@ -281,7 +283,6 @@ describe('GET /goals/:id should have dynamic currentValue ', () => {
       .then((response) => {
         const { body, status } = response;
 
-        goal2Id = body.id;
         expect(status).toBe(201);
         expect(body).toHaveProperty('id', expect.any(Number));
         expect(body).toHaveProperty('UserId', 1);
@@ -306,7 +307,6 @@ describe('GET /goals/:id should have dynamic currentValue ', () => {
       .then((response) => {
         const { body, status } = response;
 
-        goal2Id = body.id;
         expect(status).toBe(201);
         expect(body).toHaveProperty('id', expect.any(Number));
         expect(body).toHaveProperty('UserId', 1);
